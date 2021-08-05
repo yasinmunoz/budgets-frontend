106b33505b638f70b85007a840416fab83f769db
NicolasXIII <radamantiun1994@gmail.com>
2021-08-05 14:54:28 +0200

    invertir colores pipe

--- src/app/shared/pipes/p-InvertColor/p-invert-color.pipe.ts
+++ src/app/shared/pipes/p-InvertColor/p-invert-color.pipe.ts
@@ -10,8 +10,38 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PInvertColorPipe implements PipeTransform {
 
-  transform(color: any): unknown {
-    return color.complement;
+  /* transform(color: string): number {
+
+    let hex = '0X' + color.slice(1);
+    parseInt(hex);
+
+    const complementaryColor = hex - 0XFFFFFF;
+    return complementaryColor;
+  } */
+
+  transform(hex: string): string {
+    if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+      g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+      b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + this.padZero(r) + this.padZero(g) + this.padZero(b);
+  }
+
+  padZero(str:string, len:number = 2) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
   }
 
 }
